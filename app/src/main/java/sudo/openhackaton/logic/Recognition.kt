3955345e1738c3bd48c8dc626dc0ba01f775ce5b
vasilievan <vasiliev.an@edu.spbstu.ru>
2020-10-24 13:45:32 +0300

    // bm rotation fixed

--- app/src/main/java/sudo/openhackaton/logic/Recognition.kt
+++ app/src/main/java/sudo/openhackaton/logic/Recognition.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.ContentResolver
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.Matrix
 import android.net.Uri
 import android.widget.TextView
 import android.widget.Toast
@@ -17,18 +18,26 @@ import sudo.openhackaton.logic.Constants.RECOGNITION_DIDNT_SUCCEED
 import sudo.openhackaton.logic.Constants.RECOGNITION_IN_PROGRESS
 import java.io.FileOutputStream
 import java.io.IOException
-import java.lang.Exception
 
 class Recognition(private val filesLogic: FilesLogic) {
     private var inputImage: InputImage? = null
     private val recognizer = TextRecognition.getClient()
 
+    fun rotateBitmap(source: Bitmap, angle: Float): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        return Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
+    }
+
     private fun prepateData(contentResolver: ContentResolver, uri: Uri?) {
         try {
             val bm = filesLogic.getBitmapFromUri(contentResolver, uri) ?: return
+            val rotated = rotateBitmap(bm, 90f) ?: return
             filesLogic.createImageFile()
             val fos = FileOutputStream(filesLogic.lastCreated)
-            bm.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            rotated.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+            fos.flush()
+            fos.close()
         } catch (e: IOException) {
             return
         }
@@ -37,8 +46,8 @@ class Recognition(private val filesLogic: FilesLogic) {
             val src = Imgcodecs.imread(filesLogic.lastCreated!!.absolutePath, 0)
             val dst = Mat()
             Imgproc.adaptiveThreshold(
-                src, dst, 125.0, Imgproc.ADAPTIVE_THRESH_MEAN_C,
-                Imgproc.THRESH_BINARY, 11, 12.0
+                    src, dst, 125.0, Imgproc.ADAPTIVE_THRESH_MEAN_C,
+                    Imgproc.THRESH_BINARY, 11, 12.0
             )
             Imgcodecs.imwrite(filesLogic.lastCreated!!.absolutePath, dst)
         } catch (e: Exception) {
@@ -47,18 +56,18 @@ class Recognition(private val filesLogic: FilesLogic) {
     }
 
     fun doTask(
-        contentResolver: ContentResolver,
-        indicator: TextView,
-        resultCode: Int,
-        resultData: Intent?
+            contentResolver: ContentResolver,
+            indicator: TextView,
+            resultCode: Int,
+            resultData: Intent?
     ) {
         if (resultCode == Activity.RESULT_OK) {
             Toast.makeText(filesLogic.context, RECOGNITION_IN_PROGRESS, Toast.LENGTH_LONG).show()
             inputImage = if (resultData == null) {
                 if (filesLogic.bitmap != null) InputImage.fromBitmap(filesLogic.bitmap!!, 0)
                 else InputImage.fromFilePath(
-                    filesLogic.context,
-                    Uri.fromFile(filesLogic.lastCreated)
+                        filesLogic.context,
+                        Uri.fromFile(filesLogic.lastCreated)
                 )
             } else {
                 prepateData(contentResolver, resultData.data)
