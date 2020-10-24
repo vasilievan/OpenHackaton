7768cf68bebc938fb135e2d07179756f64139d9b
vasilievan <vasiliev.an@edu.spbstu.ru>
2020-10-24 22:09:33 +0300

    // Ilya! Warning!

--- app/src/main/java/sudo/openhackaton/logic/Network.java
+++ app/src/main/java/sudo/openhackaton/logic/Network.java
@@ -1,6 +1,7 @@
 package sudo.openhackaton.logic;
 
 public class Network {
+    // serialNumber and indication can be null
     public static void workForIlya(String serialNumber, String indication) {
 
     }
