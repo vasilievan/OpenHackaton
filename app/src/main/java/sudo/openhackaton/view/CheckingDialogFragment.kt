42d0dc0003a2dca77c5aa841d43a6bb0e5dbf0d2
vasilievan <vasiliev.an@edu.spbstu.ru>
2020-10-25 00:24:51 +0300

    // EditText problem was solved.

--- app/src/main/java/sudo/openhackaton/view/CheckingDialogFragment.kt
+++ app/src/main/java/sudo/openhackaton/view/CheckingDialogFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import sudo.openhackaton.R
 import sudo.openhackaton.logic.Constants.INDICATION
@@ -27,9 +29,15 @@ class CheckingDialogFragment : DialogFragment() {
         dialog!!.window?.setBackgroundDrawableResource(R.drawable.alert_background)
         val view = inflater.inflate(R.layout.dialog_check_if_text_correct, container, false)
         val indication = view.findViewById<TextView>(R.id.indication)
-        val serialNumber = view.findViewById<TextView>(R.id.serialNumber)
         indication.text = indicationString
+        indication.addTextChangedListener {
+            INDICATION_VALUE = indication.text.toString()
+        }
+        val serialNumber = view.findViewById<TextView>(R.id.serialNumber)
         serialNumber.text = serialNumberString
+        serialNumber.addTextChangedListener {
+            SERIAL_NUMBER_VALUE = serialNumber.text.toString()
+        }
         return view
     }
 
